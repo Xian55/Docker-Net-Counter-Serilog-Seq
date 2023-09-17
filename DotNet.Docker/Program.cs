c0121c942059e32a9c2417767a42271f2a7ab0ce
Xian55 <367101+Xian55@users.noreply.github.com>
2023-09-17 02:33:38 +0200

    init

---
+++ DotNet.Docker/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Exceptions;
+
+Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+    .AddEnvironmentVariables()
+    .Build();
+
+using var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.WithExceptionDetails()
+    .CreateLogger();
+
+string serverUrl = configuration.GetSection("Serilog:WriteTo:1:Args:serverUrl").Value!;
+logger.Information(serverUrl);
+
+CancellationTokenSource cts = new();
+
+AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+
+void CurrentDomain_ProcessExit(object? s, EventArgs e)
+{
+    logger.Information("Canceling...");
+    cts.Cancel();
+};
+
+int counter = 0;
+int max = args.Length is not 0 ? Convert.ToInt32(args[0]) : int.MaxValue;
+while (counter < max)
+{
+    logger.Information($"Counter: {++counter}");
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000), cts.Token);
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Docker - Seq logging - Environment Variables & appsettings.json .Net
+
+Mixing the benefits of environment variables and `appsettings.json` for .Net applications running in a Docker container
+
+# Run
+
+```
+docker compose build
+docker compose up -d
+docker compose down
+```
+
+# Seq
+
+Using http://localhost:5341 to access log
+
